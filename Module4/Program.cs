dc413a1d0ae119df112899e4de0f817189e2a304
LuanTruongg <luantruong020302@gmail.com>
2023-06-27 14:41:50 +0700

    Use the TypedResults API

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -1,7 +1,8 @@
-using Microsoft.AspNetCore.Mvc;
+ï»¿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Module4.Data;
 using Module4.Models;
+using Microsoft.AspNetCore.Mvc;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddMvc();
 
 builder.Services.AddDbContext<ToDoContext>(options => options.UseInMemoryDatabase("TodoList"));
 
@@ -29,28 +31,35 @@ app.UseAuthorization();
 
 app.MapControllers();
 
-//Sao ko xài ?c ta ??????
-//app.MapGroup("/todoitems", Group =>
-//{
+app.MapGet("/todoitems", GetAll);
+app.MapGet("/todoitems/complete", GetAllComplete);
+app.MapGet("/todoitems/{id}", GetById);
+app.MapPost("/todoitems", PostAsync);
+app.MapPut("/todoitems/{id}", PutAsync);
+app.MapDelete("/todoitems/{id}",DeleteAsync);
 
-//});
+app.Run();
 
-app.MapGet("/todoitems", async (ToDoContext _context) =>
-    await _context.ToDoItems.ToListAsync();
+static async Task<IResult> GetAll(ToDoContext _context)
+{
+    return Results.Ok(await _context.ToDoItems.ToListAsync());
+}
 
-app.MapGet("/todoitems/complete", async (ToDoContext _context) => 
-    await _context.ToDoItems.Where(x => x.IsComplete).ToListAsync());
+static async Task<IResult> GetAllComplete(ToDoContext _context)
+{
+    return Results.Ok(await _context.ToDoItems.Where(x => x.IsComplete).ToListAsync());
+}
 
-app.MapGet("/todoitems/{id}", async (int id, ToDoContext _context) =>
+static async Task<IResult> GetById(int id, ToDoContext _context)
 {
     var todoItem = await _context.ToDoItems.FindAsync(id);
     if (todoItem != null)
         return Results.Ok(todoItem);
     else
         return Results.NotFound();
-});
+}
 
-app.MapPost("/todoitems", async (ToDoContext _context, ToDoItemRequest request) =>
+static async Task<IResult> PostAsync(ToDoContext _context, [FromBody]ToDoItemRequest request)
 {
     var TodoItem = new ToDoItem
     {
@@ -60,9 +69,8 @@ app.MapPost("/todoitems", async (ToDoContext _context, ToDoItemRequest request) 
     _context.ToDoItems.Add(TodoItem);
     await _context.SaveChangesAsync();
     return Results.Created($"/todoitems/{TodoItem.Id}", TodoItem);
-});
-
-app.MapPut("/todoitems/{id}", async (ToDoContext _context, int id,[FromBody] ToDoItemRequest request) =>
+}
+static async Task<IResult> PutAsync(int id, ToDoContext _context, [FromBody] ToDoItemRequest request)
 {
     var todoItem = await _context.ToDoItems.FindAsync(id);
     if (todoItem == null)
@@ -72,25 +80,15 @@ app.MapPut("/todoitems/{id}", async (ToDoContext _context, int id,[FromBody] ToD
     _context.ToDoItems.Update(todoItem);
     await _context.SaveChangesAsync();
     return Results.Created($"/todoitems/{id}", request);
-});
+}
 
-app.MapDelete("/todoitems/{id}", async (int id, ToDoContext _context) => {
+static async Task<IResult> DeleteAsync(int id, ToDoContext _context)
+{
     var todoItem = await _context.ToDoItems.FindAsync(id);
     if (todoItem == null)
         return Results.NotFound();
     _context.ToDoItems.Remove(todoItem);
     await _context.SaveChangesAsync();
     return Results.Ok(todoItem);
-});
-
-app.Run();
-
-static async Task<IResult> GetAll(ToDoContext _context)
-{
-    return Results.Ok(await _context.ToDoItems.ToListAsync());
 }
 
-static async Task<IResult> Get(ToDoContext _context)
-{
-    return Results.Ok();
-}
