76790fd23118bc20385cd2d087d1915f3fb6b32e
LuanTruongg <luantruong020302@gmail.com>
2023-06-28 13:42:05 +0700

    Middlewares Overview

--- Module4/Controllers/ToDosController.cs
+++ Module4/Controllers/ToDosController.cs
@@ -11,76 +11,76 @@ namespace Module4.Controllers
     [ApiController]
     public class ToDosController : ControllerBase
     {
-        private readonly ToDoContext _context;        
+        //private readonly ToDoContext _context;        
 
-        public ToDosController (ToDoContext context)
-        {
-            _context = context;            
-        }
-        // GET: api/<ToDosController>
-        [HttpGet]
-        public async Task<List<ToDoItem>> Get()
-        {
-            return await _context.ToDoItems.ToListAsync();
-        }
+        //public ToDosController (ToDoContext context)
+        //{
+        //    _context = context;            
+        //}
+        //// GET: api/<ToDosController>
+        //[HttpGet]
+        //public async Task<List<ToDoItem>> Get()
+        //{
+        //    return await _context.ToDoItems.ToListAsync();
+        //}
 
-        // GET api/<ToDosController>/5
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetById(int id)
-        {
-            var result = await _context.ToDoItems.FirstOrDefaultAsync(x => x.Id == id);
-            if (result == null)
-                return NotFound();
-            return Ok(result);
-        }
+        //// GET api/<ToDosController>/5
+        //[HttpGet("{id}")]
+        //public async Task<IActionResult> GetById(int id)
+        //{
+        //    var result = await _context.ToDoItems.FirstOrDefaultAsync(x => x.Id == id);
+        //    if (result == null)
+        //        return NotFound();
+        //    return Ok(result);
+        //}
 
-        // POST api/<ToDosController>
-        [HttpPost]
-        public async Task<IActionResult> Post([FromBody] ToDoItem request)
-        {
-            if (request == null)
-                return BadRequest("Please input data");
-            var result = await _context.ToDoItems.AddAsync(request);
-            if (result == null)
-                return BadRequest("Error");
-            await _context.SaveChangesAsync();
-            return CreatedAtAction(nameof(GetById), new { id = request.Id }, request);
-        }
+        //// POST api/<ToDosController>
+        //[HttpPost]
+        //public async Task<IActionResult> Post([FromBody] ToDoItem request)
+        //{
+        //    if (request == null)
+        //        return BadRequest("Please input data");
+        //    var result = await _context.ToDoItems.AddAsync(request);
+        //    if (result == null)
+        //        return BadRequest("Error");
+        //    await _context.SaveChangesAsync();
+        //    return CreatedAtAction(nameof(GetById), new { id = request.Id }, request);
+        //}
 
-        // PUT api/<ToDosController>/5
-        [HttpPut("{id}")]
-        public async Task<IActionResult> Put(int id, [FromForm] ToDoItemRequest request)
-        {
-            if (request == null)
-                return BadRequest("Please input data");
-            var getItem = await _context.ToDoItems.FindAsync(id);
-            if (getItem == null)
-                return NotFound();
-            getItem.Id = id;
-            getItem.Name = request.Name;
-            getItem.IsComplete = request.IsComplete;
-            try
-            {
-                _context.ToDoItems.Update(getItem);
-                await _context.SaveChangesAsync();
-                return CreatedAtAction(nameof(GetById), new { id = getItem.Id }, getItem);
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                return BadRequest("Error");
-            }
-        }
+        //// PUT api/<ToDosController>/5
+        //[HttpPut("{id}")]
+        //public async Task<IActionResult> Put(int id, [FromForm] ToDoItemRequest request)
+        //{
+        //    if (request == null)
+        //        return BadRequest("Please input data");
+        //    var getItem = await _context.ToDoItems.FindAsync(id);
+        //    if (getItem == null)
+        //        return NotFound();
+        //    getItem.Id = id;
+        //    getItem.Name = request.Name;
+        //    getItem.IsComplete = request.IsComplete;
+        //    try
+        //    {
+        //        _context.ToDoItems.Update(getItem);
+        //        await _context.SaveChangesAsync();
+        //        return CreatedAtAction(nameof(GetById), new { id = getItem.Id }, getItem);
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        return BadRequest("Error");
+        //    }
+        //}
 
-        // DELETE api/<ToDosController>/5
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(int id)
-        {
-            var item = await _context.ToDoItems.FindAsync(id);
-            if (item == null)
-                return NotFound();
-            _context.ToDoItems.Remove(item);
-            await _context.SaveChangesAsync();
-            return Ok("Delete Item success");
-        }
+        //// DELETE api/<ToDosController>/5
+        //[HttpDelete("{id}")]
+        //public async Task<IActionResult> Delete(int id)
+        //{
+        //    var item = await _context.ToDoItems.FindAsync(id);
+        //    if (item == null)
+        //        return NotFound();
+        //    _context.ToDoItems.Remove(item);
+        //    await _context.SaveChangesAsync();
+        //    return Ok("Delete Item success");
+        //}
     }
 }
--- Module4/Program.cs
+++ Module4/Program.cs
@@ -8,22 +8,34 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-builder.Services.AddDbContext<ToDoContext>(options => options.UseInMemoryDatabase("TodoList"));
+//builder.Services.AddSwaggerGen();
+//builder.Services.AddDbContext<ToDoContext>(options => options.UseInMemoryDatabase("TodoList"));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    //app.UseSwagger();
+    //app.UseSwaggerUI();
 }
-SeedData.Seed(app);
+//SeedData.Seed(app);
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.Use(async (context, next) =>
+{
+    await context.Response.WriteAsync("Hello world 1");
+    await next.Invoke();
+    // Do logging or other work that doesn't write to the Response.
+});
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello world 2");
+});
 
 app.Run();
