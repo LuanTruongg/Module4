948d31aef694eb8c1aa949387a63225a5aaf65da
LuanTruongg <luantruong020302@gmail.com>
2023-06-28 16:09:27 +0700

    Branch the middleware pipeline

--- Module4/Program.cs
+++ Module4/Program.cs
@@ -20,22 +20,80 @@ if (app.Environment.IsDevelopment())
     //app.UseSwaggerUI();
 }
 //SeedData.Seed(app);
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-// app.UseCookiePolicy();
 
-app.UseRouting();
-// app.UseRateLimiter();
-// app.UseRequestLocalization();
-// app.UseCors();
+app.Map("/map1", HandleMapTest1);
 
-app.UseAuthentication();
-app.UseAuthorization();
-// app.UseSession();
-// app.UseResponseCompression();
-// app.UseResponseCaching();
+app.Map("/map2", HandleMapTest2);
 
-//app.MapRazorPages();
-app.MapDefaultControllerRoute();
+app.Map("/level1", level1App => {
+    level1App.Map("/level2a", level2a);
+    level1App.Map("/level2b", level2b);
+});
+
+//app.MapWhen(context => context.Request.Query.ContainsKey("branch"), HandleBranch);
+
+app.UseWhen(context => context.Request.Query.ContainsKey("branch"),
+    appBuilder => HandleBranchAndRejoin(appBuilder));
+
+app.Run(async context =>
+{
+    await context.Response.WriteAsync("Hello from non-Map delegate.");
+});
 
 app.Run();
+
+static void HandleMapTest1(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("Map test 1");
+    });
+}
+
+static void HandleMapTest2(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("Map test 2");
+    });
+}
+
+static void level2a(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("Map level 2a");
+    });
+}
+
+static void level2b(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        await context.Response.WriteAsync("Map level 2b");
+    });
+}
+
+static void HandleBranch(IApplicationBuilder app)
+{
+    app.Run(async context =>
+    {
+        var branchVer = context.Request.Query["branch"];
+        await context.Response.WriteAsync($"Branch used = {branchVer}");
+    });
+}
+
+void HandleBranchAndRejoin(IApplicationBuilder app)
+{
+    var logger = app.ApplicationServices.GetRequiredService<ILogger<Program>>();
+
+    app.Use(async (context, next) =>
+    {
+        var branchVer = context.Request.Query["branch"];
+        logger.LogInformation("Branch used = {branchVer}", branchVer);
+
+        // Do work that doesn't write to the Response.
+        await next();
+        // Do other work that doesn't write to the Response.
+    });
+}
